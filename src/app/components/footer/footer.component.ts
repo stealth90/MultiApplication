b188ebdac7415052e586dd6cd7e181924db0993c
stealth90 <pierantonio.petralia@live.it>
2021-04-10 13:29:07 +0200

    fix footer cmponent for background color and size

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
-import { Subscription } from 'rxjs';
+import { fromEvent, Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-footer',
@@ -9,19 +17,48 @@ import { Subscription } from 'rxjs';
 })
 export class FooterComponent implements OnInit, OnDestroy {
   currentLanguage: string;
-  subscription: Subscription;
-  constructor(private translate: TranslateService) {}
+  subscriptions: Subscription[] = [];
+  offsetFooter: boolean = false;
+  currentRoute: string;
+
+  constructor(private translate: TranslateService, private router: Router) {
+    const router$ = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute = event.url.slice(1);
+      });
+    this.subscriptions.push(router$);
+  }
+
+  @ViewChild('footer', { read: ElementRef, static: true }) footer: ElementRef;
 
   ngOnInit(): void {
-    this.subscription = this.translate.onLangChange.subscribe(
+    const lang$ = this.translate.onLangChange.subscribe(
       (event: LangChangeEvent) => {
         this.currentLanguage = event.lang;
       }
     );
+    const footerHeight$ = fromEvent(window, 'scroll').subscribe(() => {
+      const footerHeight = this.footer.nativeElement.clientHeight;
+      const body = document.body,
+        html = document.documentElement;
+      const height = Math.max(
+        body.scrollHeight,
+        body.offsetHeight,
+        html.clientHeight,
+        html.scrollHeight,
+        html.offsetHeight
+      );
+      const windowHeight =
+        window.pageYOffset + window.innerHeight > height - footerHeight / 2;
+      this.offsetFooter = !windowHeight;
+    });
+
+    this.subscriptions.push(lang$, footerHeight$);
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 
   changeLanguage(lang: string) {
