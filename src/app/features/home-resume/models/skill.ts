7ce1a5977e6e4727c37245cbcb334bbce153c8be
stealth90 <pierantonio.petralia@live.it>
2021-04-11 12:49:52 +0200

    adds all skills image and create skill interface

---
+++ src/app/features/home-resume/models/skill.ts
@@ -0,0 +1,32 @@
+export interface Skill {
+  name: string;
+  percentage: number;
+  icon: string;
+  /* readOnly: boolean;
+  size: number;
+  unit: string;
+  textColor: string;
+  fontSize: string;
+  fontWeigth: string;
+  fontFamily: string;
+  valueformat: string;
+  value: number;
+  iconSize: number;
+  iconColor: string;
+  max: number;
+  trackWidth: number;
+  barWidth: number;
+  trackColor: string;
+  barColor: string;
+  subText?: SubText; */
+}
+
+/* export interface SubText {
+  enabled: boolean;
+  fontFamily: string;
+  font: string;
+  fontWeight: string;
+  text: string;
+  color: string;
+  offset: number;
+} */
