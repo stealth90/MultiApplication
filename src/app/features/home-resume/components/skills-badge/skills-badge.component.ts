540eef48b92adc957fa447db301ac3beb0967c04
stealth90 <pierantonio.petralia@live.it>
2021-04-11 12:50:18 +0200

    create skills badge component

---
+++ src/app/features/home-resume/components/skills-badge/skills-badge.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Skill } from '../../models/skill';
+
+@Component({
+  selector: 'skills-badge',
+  templateUrl: './skills-badge.component.html',
+  styleUrls: ['./skills-badge.component.scss'],
+})
+export class SkillsBadgeComponent implements OnInit {
+  @Input() skill: Skill;
+  @Input() id: number;
+  knobOptions = {
+    readOnly: true,
+    size: 200,
+    valueformat: 'percent',
+    max: 100,
+    trackWidth: 9,
+    barWidth: 6,
+    trackColor: '#000',
+    barColor: '#f14336',
+  };
+  constructor() {}
+
+  ngOnInit(): void {}
+}
